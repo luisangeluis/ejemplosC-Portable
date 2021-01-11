38d5e213cd0b1b2019e87cddfa3c46461e5fdcfb
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-01-11 17:20:39 -0600

    Avance del ejemplo dispensadora de alimentos

--- cs/DispensadoraDeAlimentos.cs
+++ cs/DispensadoraDeAlimentos.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
@@ -27,10 +29,59 @@ class CDispensadora{
     public List<CProducto> Productos {set; get;}
     public string Pago{set; get;}
     
-    pubcli CDispensadora(){
-        CProducto cocaCola = new CProducto();
+    public CDispensadora(){
+        CProducto coca = new CProducto();
         coca.Codigo="01";
+        coca.Nombre="coca Cola";
+        coca.Categoria = "B";
+        coca.Cantidad = 5;
+        coca.Valor = 2000;
         
+        CProducto papas = new CProducto();
+        papas.Codigo="02";
+        papas.Nombre="papas";
+        papas.Categoria = "M";
+        papas.Cantidad = 2;
+
+        papas.Valor = 1500;
+        
+        Productos.Add(coca);
+        Productos.Add(papas);
+
+        
+    }
+    
+    public int validarProducto(string pCodigo){
+        int posicion=-1;
+        
+        for(int x=0; x<Productos.Count;x++){
+            if(Productos[x].Codigo==pCodigo){
+                posicion = x;
+            }
+        }
+        
+        return posicion;
+        
+        
+    }
+    
+    public bool agregarProducto(CProducto pProducto){
+        
+        int posicion =-1; 
+        posicion = validarProducto(pProducto.Codigo);
+        
+        if(posicion>=0){
+            
+            //Revisar el metodo que usaron en el curso
+            /*****************************************/
+            /*****************************************/
+            Productos[posicion].Cantidad +=1;
+        }else{
+            Productos.Add(pProducto);
+
+        }
+        
+        return true;
     }
     
     
